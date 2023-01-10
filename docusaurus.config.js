de46430ffa5186911190e1e2996f29cae16fe3af
biohackerellie <epkerns@gmail.com>
2023-01-10 18:03:25 +0000

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// const jQuery = require('http://code.jquery.com/jquery-latest.js');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -22,6 +23,11 @@ const config = {
   //   defaultLocale: 'en',
   //   locales: ['en'],
   // },
+  scripts:[
+      'https://laurelschools.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/1jmxwi/b/8/b0105d975e9e59f24a3230a22972a71a/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=9cb68c49',
+
+    
+  ],
 
   presets: [
     [
@@ -37,6 +43,8 @@ const config = {
       }),
     ],
   ],
+
+
   
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -74,7 +82,7 @@ const config = {
 
       footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Laurel Public Schools. Built by <a href=\"https://github.io/boihackerellie\">Ellie Kerns</a>` ,
+      copyright: `Copyright © ${new Date().getFullYear()} Laurel Public Schools. Built by <a href=\"https://github.com/boihackerellie\">Ellie Kerns</a>` ,
       logo: {
           alt: 'LPS',
           src: 'img/LPS.svg',
