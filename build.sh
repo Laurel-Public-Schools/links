15f161b8abe690742606786ce9d423f6271a216b
biohackerellie <epkerns@gmail.com>
2023-03-23 11:52:21 -0600

    updated build script

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ if ! git diff --quiet HEAD @{u}; then
     # Deletes old container
     docker system prune -f && \
     # Restart container with new image
-    docker run --rm --name $CONTAINER_NAME -p 3000:3000 -d $IMAGE_NAME:latest
+    docker run --name $CONTAINER_NAME -p 3000:3000 -d $IMAGE_NAME:latest
 
 else
     echo "No changes. Exiting."
